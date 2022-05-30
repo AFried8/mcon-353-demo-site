a641e5104dd485c3f7b4d61714ebebfebdf7d0d2
AFried8 <78141118+AFried8@users.noreply.github.com>
2022-05-29 22:06:19 -0400

    Minor changes

--- src/components/todoEdit/todoEdit.jsx
+++ src/components/todoEdit/todoEdit.jsx
@@ -73,7 +73,7 @@ export const TodoEdit = () => {
 };
 
 const TodoItem = (props) => {
-    const colors = props.checked?  ['#C8C8C8',  '#000000']: ['#FFFFFF',  '#560ba6'];
+    const colors = props.checked?  ['#FFFFFF',  '#560ba6']: ['#C8C8C8',  '#000000'];
     
     function toggleCheck(index){
         props.check(index);
